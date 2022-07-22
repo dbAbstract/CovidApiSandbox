c9b93ac513ec7c590f4eecd147697a47f38be173
Taqi Enari <bmtaki335@gmail.com>
2022-07-22 14:42:15 +0900

    - added exception class - created repo interface - created API Singleton - Gradle sorted out for proj dependencies - Screen + ViewModel for screen created

--- app/src/main/java/com/example/volleysandbox/repository/AppRepository.kt
+++ app/src/main/java/com/example/volleysandbox/repository/AppRepository.kt
@@ -1,7 +1,20 @@
 package com.example.volleysandbox.repository
 
+import com.example.volleysandbox.data.DataOrException
+import com.example.volleysandbox.model.CountryStatusItem
 import com.example.volleysandbox.network.CovidApi
 import javax.inject.Inject
 
 class AppRepository @Inject constructor(private val api: CovidApi) {
+
+    // Wrapping response object list in dataOrException for exception handling
+    private val dataOrException = DataOrException<ArrayList<CountryStatusItem>, Boolean, Exception>()
+
+    suspend fun getByCountryAndStatus() : DataOrException<ArrayList<CountryStatusItem>, Boolean, Exception>() {
+        try {
+            dataOrException.loading = true
+        } catch(ex: Exception) {
+
+        }
+    }
 }
